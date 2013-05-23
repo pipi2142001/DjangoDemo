7bca32bb310d0bee8d9811d9e59220d59a1f6c8b
pipi2142001 <5748863@qq.com>
2013-05-23 17:19:50 +0800

    页面、模板

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,4 @@
+# -*-coding:utf-8-*- 
 from django.contrib import admin
 from polls.models import Poll
 from polls.models import Choice
@@ -15,7 +16,9 @@ class PollAdmin(admin.ModelAdmin):
     ]
     inlines = [ChoiceInline]
     list_display = ('question', 'pub_date', 'was_published_recently')   
-    
+    list_filter = ['pub_date']
+    search_fields = ['question']
+    date_hierarchy = 'pub_date'
      
 #admin.site.register(Poll)
 #admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,4 @@
+# -*-coding:utf-8-*- 
 from django.db import models
 import datetime
 from django.utils import timezone
@@ -6,11 +7,18 @@ from django.utils import timezone
 class Poll(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    
+    #类似于java的toString方法。有了该方法，在django打印查询到的models对象时，会显示该方法返回的值
     def __unicode__(self):
         return self.question
+    
+    
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently? '
+     
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
     choice_text = models.CharField(max_length=200)
